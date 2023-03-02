f5e0e2757013e4286c3b0673720a8cc11d90f6fd
Andrew Bullis <abullis@jmesales.com>
2023-03-02 13:49:00 -0600

    Add ofd label and unclutter the main function

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@
             openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
             openFileDialog.FilterIndex = 2;
             openFileDialog.RestoreDirectory = true;
+            openFileDialog.Title = "Choose the TXT file of comma-separated items";
 
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
@@ -26,40 +27,41 @@
                 using (StreamReader reader = new StreamReader(fileStream))
                 {
                     fileContent = reader.ReadToEnd();
-                    // Our original comma-separated text goes into one array
-                    string[] contentArray = fileContent.Split(",");
-                    // And will be sorted into another array in a different order
-                    string[] newList = new string[contentArray.Length];
-                    string newString = "";
-
-                    for (int i = 0; i < contentArray.Length; i++)
-                    {
-                        Random rnd = new Random();
-                        int place = rnd.Next(contentArray.Length);
-
-                        // Here we ensure that no value is overwritten, as that
-                        // would result in a loss of data
-                        while (newList[place] != null)
-                        {
-                            place = rnd.Next(contentArray.Length);
-                        }
-
-                        newList[place] = contentArray[i];
-                    }
-
-                    for (int i = 0; i < newList.Length; i++)
-                    {
-                        newString += newList[i] + ",";
-                    }
-
-                    // Ideally, and I should do this if I need this tool more
-                    // than just this one time, I would make a new TXT file and
-                    // write this in there. But, since I only need to do this
-                    // this one time, I'll just copy the output from the VS
-                    // debug console.
-                    Console.WriteLine(newString);
                 }
             }
         }
+                
+        // Our original comma-separated text goes into one array
+        string[] contentArray = fileContent.Split(",");
+        // And will be sorted into another array in a different order
+        string[] newList = new string[contentArray.Length];
+        string newString = "";
+
+        for (int i = 0; i < contentArray.Length; i++)
+        {
+            Random rnd = new Random();
+            int place = rnd.Next(contentArray.Length);
+
+            // Here we ensure that no value is overwritten, as that
+            // would result in a loss of data
+            while (newList[place] != null)
+            {
+                place = rnd.Next(contentArray.Length);
+            }
+
+            newList[place] = contentArray[i];
+        }
+
+        for (int i = 0; i < newList.Length; i++)
+        {
+            newString += newList[i] + ",";
+        }
+
+        // Ideally, and I should do this if I need this tool more
+        // than just this one time, I would make a new TXT file and
+        // write this in there. But, since I only need to do this
+        // this one time, I'll just copy the output from the VS
+        // debug console.
+        Console.WriteLine(newString);
     }
 }
